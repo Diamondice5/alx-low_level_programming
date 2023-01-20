3f2f4089d298fab37a67fdf82d1b56c9982a3a30
Diamondice5 <gospenoch@gmail.com>
2023-01-20 03:52:44 -0800

    Diamond

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,19 +5,11 @@
 
 
 /**
-
  * print_name - funtion entry point
-
  * @name: str to print
-
  * @f: function pointer
-
  * Desc: a function that prints a name
-
- *
-
  * Return: void
-
  */
 
 void print_name(char *name, void (*f)(char *))
